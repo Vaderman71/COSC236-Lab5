dc03d7abe03fb6820edd10bed9625d14d9167755
LordZorgath <52773168+LordZorgath@users.noreply.github.com>
2025-11-03 14:34:38 -0800

    Implement Library.java

--- Library.java
+++ Library.java
@@ -2,15 +2,45 @@ package lab4.library;
 
 public class Library {
 	
-	//  GRASP Principles: Creator, Controller
-	//	Manage the catalog of books and members.
-	//	Track which books are borrowed and available
+	private List<Book> bookList;
 	
+	private List<Member> memberList;
 	
-	// Attributes:
-	// private catalog (list of Book) 
-	// private members (list of Members)
+	public void addBook(Book b){
+		bookList.add(b);
+	}
+	
+	public void registerMember(Member m){
+		memberList.add(m);
+	}
+	
+	public void showBooks(){
+		for(int i = 0; i < bookList.size(); i++){
+			System.out.println(book.get(i));
+		}
+	}
+	
+	public void showMembers(){
+		for(int i = 0; i < bookList.size(); i++){
+			System.out.println(members.get(i));
+		}
+	}
+	
+	public Book findBookByTitle(String title){
+		for(int i = 0; i < bookList.size(); i++){
+			if(bookList.at(i).getTitle().equals(title)){
+				return bookList.at(i);
+			}
+		}
+		return null;
+	}
 
-	// TODO: implement functionality of Member class
-
+	public Member findMemberByName(String name){
+		for(int i = 0; i < MemberList.size(); i++){
+			if(MemberList.at(i).getName().equals(name)){
+				return MemberList.at(i);
+			}
+		}
+		return null;
+	}
 }
