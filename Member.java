f64485ac35c266053b932e3564c6a9517cf42a18
Vaderman71 <ethan.turchyn@gmail.com>
2025-11-03 13:02:58 -0800

    Completed Member.java
    I added all the member info from the class diagram to the member.java file.

--- Member.java
+++ Member.java
@@ -1,13 +1,49 @@
 package lab4.library;
 
 public class Member {
+    private static int nextId = 1;
 
-	// GRASP Principle: Information Expert and Low Coupling
-	// Can borrow and return books.
+    private int memberId;
+    private String name;
+    private List<Book> borrowedBooks;
 
-	private String name;
-	// private borrowedbBooks TODO: implement collection of borrowed books
-	
-	// TODO: implement functionality of Member class
+    public Member(String name) {
+        this.memberId = nextId++;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
 
+    public int getMemberId() { return memberId; }
+    public String getName() { return name;}
+    public List<Book> getBorrowedBooks() { return borrowedBooks; }
+
+    public boolean borrowBook(Book book) {
+        if (book.isAvailable()) {
+            borrowedBooks.add(book);
+            book.setAvailable(false);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean returnBook(Book book) {
+        if (borrowedBooks.contains(book)) {
+            borrowedBooks.remove(book);
+            book.setAvailable(true);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasBook(Book book) {
+        return borrowedBooks.contains(book);
+    }
+
+    public List<String> getBorrowedBookTitles() {
+        List<String> titles = new ArrayList<>();
+        for (Book book : borrowedBooks) {
+            titles.add(book.getTitle());
+        }
+        return titles;
+    }
 }
